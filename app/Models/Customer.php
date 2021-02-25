0cbaf329f05ccb7b8b11f234425382a9d1db2770
Jarek Goszowski <goszowski@gmail.com>
2021-02-25 18:27:51 +0200

    Інтеграція UUID для розприділеної системи

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use GoldSpecDigital\LaravelEloquentUUID\Foundation\Auth\User as Authenticatable;
 
 class Customer extends Authenticatable implements JWTSubject
 {
--- database/migrations/2014_10_12_000000_create_customers_table.php
+++ database/migrations/2014_10_12_000000_create_customers_table.php
@@ -14,7 +14,7 @@ class CreateCustomersTable extends Migration
     public function up()
     {
         Schema::create('customers', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('lastname');
             $table->string('firstname');
             $table->string('patronymic');
